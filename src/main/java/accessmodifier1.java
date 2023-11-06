daf5347c69b2991ae7d2733464aaf080d7a98e53
logesh <lgesh818@gmail.com>
2023-11-05 20:34:46 -0500

    new accessmodifier1

--- src/main/java/accessmodifier1.java
+++ src/main/java/accessmodifier1.java
@@ -5,6 +5,9 @@ public class accessmodifier1 {
     void singapore(){
         System.out.println("singapore");
     }
+    public void thailand(){
+        System.out.println("thailand");
+    }
     public static void main(String args[]){
         accessmodifier1 ac1=new accessmodifier1();
         ac1.singapore();
